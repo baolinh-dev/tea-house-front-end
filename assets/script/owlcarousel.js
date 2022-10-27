020a33e72b581fe74110f1a37966fd63ae73f8af
bao linh <baolinhdev@gmail.com>
2022-10-27 08:35:11 +0700

    html detail

--- assets/script/owlcarousel.js
+++ assets/script/owlcarousel.js
@@ -67,5 +67,21 @@ $('#menu .owl-carousel').owlCarousel({
             items:4
         }
     }
-})  
+})   
+$('#detail .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,   
+     responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+}) 
 
